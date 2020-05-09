458b88d03600aff1d4080d7115d3c2498fbac7de
son1cn <31121758+son1cn@users.noreply.github.com>
2020-05-09 17:52:36 +0930

    fixed library links

--- FlickLink.py
+++ FlickLink.py
@@ -6,16 +6,22 @@ import time
 
 lib_dir = os.path.normpath(os.path.dirname(os.path.realpath(__file__)) + '/modules/link-python/lib/')
 sys.path.insert(0, lib_dir)
+print(lib_dir)
 
 try:
     import link
 except ImportError:
     exit("link is not properly installed")
 
+
+lib_dir = os.path.normpath(os.path.dirname(os.path.realpath(__file__)) + '/modules/Flick/flick/')
+sys.path.insert(0, lib_dir)
+print(lib_dir)
+
 try:
     import flicklib
 except ImportError:
-    exit("link is not properly installed")
+    exit("Flick is not properly installed")
 
 import signal
 import time
